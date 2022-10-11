e71108d40eb7145a7ebd82145007be1e94241dee
Eduardo Ronchi <financaronchi@gmail.com>
2022-10-11 11:50:53 -0300

    Obtendo um usuario por id

--- src/main/java/com/ronchiedu/workshopmongo/resources/UserResource.java
+++ src/main/java/com/ronchiedu/workshopmongo/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,16 +15,22 @@ import com.ronchiedu.workshopmongo.dto.UserDTO;
 import com.ronchiedu.workshopmongo.services.UserService;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value="/users")
 public class UserResource {
 
 	@Autowired
 	private UserService service;
 
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 }
--- src/main/java/com/ronchiedu/workshopmongo/services/UserService.java
+++ src/main/java/com/ronchiedu/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.ronchiedu.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ronchiedu.workshopmongo.domain.User;
 import com.ronchiedu.workshopmongo.repository.UserRepository;
+import com.ronchiedu.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,10 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+	}
+
