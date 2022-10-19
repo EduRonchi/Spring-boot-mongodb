d49c9ed7ab441e985d53981daf19b275ad667462
Eduardo Ronchi <financaronchi@gmail.com>
2022-10-19 11:46:02 -0300

    Consulta simples com query methods

--- src/main/java/com/ronchiedu/workshopmongo/resources/PostResource.java
+++ src/main/java/com/ronchiedu/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.ronchiedu.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ronchiedu.workshopmongo.domain.Post;
+import com.ronchiedu.workshopmongo.resources.util.URL;
 import com.ronchiedu.workshopmongo.services.PostService;
 
 @RestController
@@ -24,4 +28,10 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue= "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
 }
--- src/main/java/com/ronchiedu/workshopmongo/services/PostService.java
+++ src/main/java/com/ronchiedu/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.ronchiedu.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,10 @@ public class PostService {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
+	
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 	
 }
 
