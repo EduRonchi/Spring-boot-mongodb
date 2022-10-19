7c6f994c1315681e0c57280f30db73d3e57f88a7
Eduardo Ronchi <financaronchi@gmail.com>
2022-10-19 01:09:19 -0300

    Referenciando os posts do usuario

--- src/main/java/com/ronchiedu/workshopmongo/config/Instantiation.java
+++ src/main/java/com/ronchiedu/workshopmongo/config/Instantiation.java
@@ -44,6 +44,10 @@ public class Instantiation implements CommandLineRunner {
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
+		
+		
 	}
 
 }
--- src/main/java/com/ronchiedu/workshopmongo/domain/User.java
+++ src/main/java/com/ronchiedu/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.ronchiedu.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
@@ -14,6 +17,10 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
+		
 	
 	public User() {
 	}
@@ -48,6 +55,14 @@ public class User implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
 
 	@Override
 	public int hashCode() {
@@ -65,6 +80,8 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
+	
 
 	
 }
